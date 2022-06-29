a56bc0064eed8ec568f99a4393ffa1756e1be127
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-29 18:08:26 -0300

    feat: implementa função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,6 @@
+import datetime
+import re
+
 from tech_news.database import db
 
 
@@ -14,8 +17,54 @@ def search_by_title(title):
 
 
 # Requisito 7
+def translate_month(month):
+    month_br = {
+        "January": "janeiro",
+        "February": "fevereiro",
+        "March": "março",
+        "April": "abril",
+        "May": "maio",
+        "June": "junho",
+        "July": "julho",
+        "August": "agosto",
+        "September": "setembro",
+        "October": "outubro",
+        "November": "novembro",
+        "December": "dezembro",
+    }
+    try:
+        result = month_br[month]
+    except KeyError:
+        result = month
+    return result
+
+
+def validate_date(date_str):
+    try:
+        date = datetime.date.fromisoformat(date_str)
+    except ValueError:
+        raise(ValueError("Data inválida"))
+    return date
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    if not validate_date(date):
+        raise(ValueError("Data inválida"))
+    month = datetime.date.fromisoformat(date).strftime("%B")
+    month_br = translate_month(month)
+    day = datetime.date.fromisoformat(date).strftime("%d").removeprefix("0")
+    year = datetime.date.fromisoformat(date).strftime("%Y")
+    search_date = f"{day} de {month_br} de {year}"
+    query = db.news.find(
+        {"timestamp": search_date},
+        {"_id": False, "title": True, "url": True})
+    news_list = list(query)
+    result = []
+    for news in news_list:
+        result.append((news["title"], news["url"]))
+    print("\n", date)
+    print("\n", result)
+    return result
 
 
 # Requisito 8
